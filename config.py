f33e0db025ee91dc557a5063a4d967785662b1d4
Anas <anasdhaoidi001@gmail.com>
2025-12-18 12:38:34 +0100

    Change session type to 'ffa' and update playlist code

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # see bombsquad_server for details on what you can override
 # examples (uncomment to use):
  config['partyName'] = '|ARABIAN SERVER BY ATD JOIN|'
- config['sessionType'] = 'teams'
+ config['sessionType'] = 'ffa'
  config['maxPartySize'] = 15
  config['port'] = 43210
- config['playlistCode'] = 43219
+ config['playlistCode'] = 575884
