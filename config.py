c576029fbf2bd58b3b5188ad429906a8db6508b8
Anas <anasdhaoidi001@gmail.com>
2025-12-24 15:59:18 +0100

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# place any of your own overrides here. 
-# see bombsquad_server for details on what you can override
-# examples (uncomment to use):
- config['partyName'] = '|ARABIAN SERVER BY ATD JOIN|'
- config['sessionType'] = 'ffa'
- config['maxPartySize'] = 15
- config['port'] = 43210
- config['playlistCode'] = 575884
